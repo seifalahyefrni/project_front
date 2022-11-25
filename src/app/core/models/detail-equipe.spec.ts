7ce72dd21db0a30aa2277df3d34ea67933110791
seifallahyefrni <118931624+seifalahyefrni@users.noreply.github.com>
2022-11-25 22:28:58 +0100

    first commit

---
+++ src/app/core/models/detail-equipe.spec.ts
@@ -0,0 +1,7 @@
+import { DetailEquipe } from './detail-equipe';
+
+describe('DetailEquipe', () => {
+  it('should create an instance', () => {
+    expect(new DetailEquipe()).toBeTruthy();
+  });
+});
